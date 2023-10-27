56a629d392a68b243b076f422e84aaa29e6f3916
PaolaMarquez5784 <135775324+PaolaMarquez5784@users.noreply.github.com>
2023-10-27 18:16:49 -0400

    Add files via upload

---
+++ Teleferico.pas
@@ -0,0 +1,60 @@
+Program TelefericoMerida;
+uses crt;
+var
+	BoletosV, BoletosC, cantMax, asientosD,edadAdultos, edadNinos, cedula, cantBoletos, op: integer;
+	estaciones, tramos: string;
+	nombre, apellido, estacion, tramo, tipoBoleto: string;
+	montoTotal, precioBoletos: real;
+begin
+	repeat
+		writeln('**********************************************');
+		writeln('BIENVENIDO AL SISTEMA DEL TELEFERCIO DE MÉRIDA');
+		writeln('**********************************************');
+		writeln(' ');
+		writeln('---------- MENU PRINCIPAL ----------');
+		writeln('1. Comprar Boletos');
+		writeln('2. Ver Sistema');
+		writeln('3. Salir');
+		writeln('------------------------------------');
+		readln(op);
+		
+		case op of
+		clrscr;
+			'1': begin
+				writeln('Para continuar con su transaccion, ingrese los siguientes datos: ');
+				write('Nombre: ');
+				readln(nombre);
+				
+				write('Apellido: ');
+				readln(apellido);
+			
+				write('Numero de Cedula: ');
+				readln(cedula);
+				
+				write('Cuantos boletos desea comprar?: ');
+				readln(cantBoletos);
+				
+				clrscr;
+				writeln('Niombre: ', nombre);
+				writeln('Apellido: ', apellido);
+				writeln('Numero de cedula: ', cedula);
+				
+				repeat
+					writeln('Indique la estacion de la que desea realizar su viaje');
+					writeln('a) Barinitas');
+					writeln('b) La montaña');
+					writeln('c) La Aguada');
+					writeln('d) Loma Redonda');
+					writeln('e) Pico Espejo');
+					write('Estacion: ');
+					readln(estaciones);
+					
+				until
+			end;
+			
+
+		
+	until op=3;
+		
+		writeln('GRACIAS POR SU VISITA, VUELVA PRONTO!');
+end.
