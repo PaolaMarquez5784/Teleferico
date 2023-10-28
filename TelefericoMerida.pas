9d9c2967a048241e07606a0cdaa9bb49196ddda2
PaolaMarquez5784 <135775324+PaolaMarquez5784@users.noreply.github.com>
2023-10-27 22:42:41 -0400

    Update TelefericoMerida.pas

--- TelefericoMerida.pas
+++ TelefericoMerida.pas
@@ -6,9 +6,9 @@ const
 	PrecioEspecial = 12;
 var
 	BoletosV, BoletosC, asientosD,edadAdultos, edadNinos, cantBoletos, op: integer;
-	estaciones, tramos: string;
-	nombre, apellido, cedula, tramo, tipoBoleto: string;
-	montoTotal, precioBoletos: real;
+	estaciones, tramos, tipoBoleto: string;
+	nombre, apellido, cedula: string;
+	montoTotal, precioTotalBoleto: real;
 begin
 	cantBoletos := 0; //Inicializamos la cantidad de boletos en cero.
 	
@@ -34,32 +34,79 @@ begin
 				writeln('||************************************************************||');
 				
 				writeln(' ');
-				write('Cuantos boletos desea comprar?: ');
+				write('|| Cuantos boletos desea comprar?: ');
 				readln(cantBoletos);
 				
 				if (cantBoletos <= 60) then
 					begin
-						writeln('||------------------------------||');
-						writeln('|| SELECCIONE EL TIPO DE BOLETO ||');
-						writeln('||------------------------------||');
-						writeln('|| 1. Boleto General            ||');
-						writeln('|| 2. Boleto Especial           ||');
-						writeln('||------------------------------||');
+						clrscr;
+						writeln('||------------------------------------------------------------||');
+						writeln('||                 SELECCIONE EL TIPO DE BOLETO               ||');
+						writeln('||------------------------------------------------------------||');
+						writeln('|| 1. Boleto General - Ida/vuelta. {PRECIO: $40.00}           ||');
+						writeln('|| 2. Boleto General - Solo ida. {PRECIO: $20.00}             ||');
+						writeln('|| 3. Boleto Especial - Ida/vuelta. {PRECIO: $24.00}          ||');
+						writeln('|| 4. Boleto Especial - Solo ida. {PRECIO: $12.00}            ||');
+						writeln('||------------------------------------------------------------||');
 						write('|| Ingrese el numero de su eleccion y presione ENTER: ');
 						readln(tipoBoleto);
-      
-						writeln('---------------------------------------------------------------');
-						writeln('Para continuar con su transaccion, ingrese los siguientes datos');
-						writeln('---------------------------------------------------------------');
-						write('Nombre: ');
+																	 
+						case tipoBoleto of
+							'1': begin
+								textcolor(Yellow);
+								writeln('||------------------------------------------------------------||');
+								writeln('||Usted selecciono la opcion 1. Boleto General - Ida/vuelta.  ||');
+								//calculamos el costo total segun la cantidad de boletos
+								precioTotalBoleto := (40 * cantBoletos);
+								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+								writeln('||------------------------------------------------------------||');
+							end;
+							
+							'2': begin
+								textcolor(Yellow);
+								writeln('||------------------------------------------------------------||');
+								writeln('||Usted selecciono la opcion 2. Boleto General - Solo ida.    ||');
+								//calculamos el costo total segun la cantidad de boletos
+								precioTotalBoleto := (20 * cantBoletos);
+								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+								writeln('||------------------------------------------------------------||');
+							end;
+							
+							'3': begin
+								textcolor(Yellow);
+								writeln('||------------------------------------------------------------||');
+								writeln('|| Usted selecciono la opcion 3. Boleto Especial - Ida/vuelta.||');
+								//calculamos el costo total segun la cantidad de boletos
+								precioTotalBoleto := (24 * cantBoletos);
+								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+								writeln('||------------------------------------------------------------||');
+							end;
+							
+							'4': begin
+								textcolor(Yellow);
+								writeln('||------------------------------------------------------------||');
+								writeln('|| Usted selecciono la opcion 4. Boleto Especial - Solo ida.  ||');
+								//calculamos el costo total segun la cantidad de boletos
+								precioTotalBoleto := (12 * cantBoletos);
+								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+								writeln('||------------------------------------------------------------||');
+							end;
+						end;
+						
+						delay(5000);
+						clrscr;
+						writeln('||-----------------------------------------------------------------||');
+						writeln('|| Para continuar con su transaccion, ingrese los siguientes datos ||');
+						writeln('||-----------------------------------------------------------------||');
+						write('||Nombre: ');
 						readln(nombre);
 						
 						writeln(' ');
-						write('Apellido: ');
+						write('||Apellido: ');
 						readln(apellido);
 						
 						writeln(' ');
-						write('Numero de Cedula: ');
+						write('||Numero de Cedula: ');
 						readln(cedula);
 						
 						clrscr;
@@ -69,6 +116,7 @@ begin
 						writeln('|| Nombre: ', nombre);
 						writeln('|| Apellido: ', apellido);
 						writeln('|| Numero de cedula: ', cedula);
+						writeln('||--------------------------||');
 						delay (3000);
 						
 						repeat
