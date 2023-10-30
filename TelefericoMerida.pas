2d1b633d8917fa09430699180835714549d02333
PaolaMarquez5784 <135775324+PaolaMarquez5784@users.noreply.github.com>
2023-10-30 00:41:48 -0400

    Update TelefericoMerida.pas

--- TelefericoMerida.pas
+++ TelefericoMerida.pas
@@ -2,155 +2,305 @@ Program TelefericoMerida;
 uses crt;
 const
 	cantMaxPasajeros = 60;
+	boletoGeneral = 20;
+	boletoEspecial = 12;
 var
-	BoletosV, BoletosC, asientosD,edadAdultos, edadNinos, cantBoletos, op: integer;
-	estaciones, tramos, tipoBoleto: string;
+	BoletosV, BoletosC, asientosD, edad, cantBoletos, op: integer;
+	estaciones, tramos, tipoBoleto, resp: string;
 	nombre, apellido, cedula: string;
 	montoTotal, precioTotalBoleto: real;
 begin
 	cantBoletos := 0; //Inicializamos la cantidad de boletos en cero.
-	
-		writeln('||*************************************************||');
-		writeln('||  BIENVENIDO AL SISTEMA DEL TELEFERICO DE MERIDA ||');
-		writeln('||*************************************************||');
+		
+		textcolor(yellow);
+		writeln('||********************************************************||');
+		writeln('||     BIENVENIDO AL SISTEMA DEL TELEFERICO DE MERIDA     ||');
+		writeln('||********************************************************||');
 	repeat
-		writeln('||-------------------------------------------------||');
-		writeln('||----------------- MENU PRINCIPAL ----------------||');
-		writeln('|| 1. Comprar Boletos                              ||');
-		writeln('|| 2. Ver Sistema                                  ||');
-		writeln('|| 3. Salir                                        ||');
-		writeln('||-------------------------------------------------||');
+		textcolor(white);
+		writeln(' ');
+		writeln('||---------------------------------------------||');
+		writeln('||--------------- MENU PRINCIPAL --------------||');
+		writeln('|| 1. Comprar Boletos                          ||');
+		writeln('|| 2. Ver Sistema                              ||');
+		writeln('|| 3. Salir                                    ||');
+		writeln('||---------------------------------------------||');
 		write('|| Indique su opcion y presione enter: ');
 		readln(op);
 		delay (1000);
 		
 		if op = 1 then
-			begin
-			clrscr;
-				writeln('||************************************************************||');
-				writeln('|| AVISO: La capacidad maxima de pasajeros por viaje es de ', cantMaxPasajeros, ' ||');
-				writeln('||************************************************************||');
-				
-				writeln(' ');
-				write('|| Cuantos boletos desea comprar?: ');
-				readln(cantBoletos);
-				
-				if (cantBoletos <= 60) then
-					begin
-						clrscr;
-						writeln('||------------------------------------------------------------||');
-						writeln('||                 SELECCIONE EL TIPO DE BOLETO               ||');
-						writeln('||------------------------------------------------------------||');
-						writeln('|| 1. Boleto General - Ida/vuelta. {PRECIO: $40.00}           ||');
-						writeln('|| 2. Boleto General - Solo ida. {PRECIO: $20.00}             ||');
-						writeln('|| 3. Boleto Especial - Ida/vuelta. {PRECIO: $24.00}          ||');
-						writeln('|| 4. Boleto Especial - Solo ida. {PRECIO: $12.00}            ||');
-						writeln('||------------------------------------------------------------||');
-						write('|| Ingrese el numero de su eleccion y presione ENTER: ');
-						readln(tipoBoleto);
-																	 
-						case tipoBoleto of
-							'1': begin
-								textcolor(Yellow);
-								writeln('||------------------------------------------------------------||');
-								writeln('||Usted selecciono la opcion 1. Boleto General - Ida/vuelta.  ||');
-								//calculamos el costo total segun la cantidad de boletos
-								precioTotalBoleto := (40 * cantBoletos);
-								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
-								writeln('||------------------------------------------------------------||');
-							end;
-							
-							'2': begin
-								textcolor(Yellow);
-								writeln('||------------------------------------------------------------||');
-								writeln('||Usted selecciono la opcion 2. Boleto General - Solo ida.    ||');
-								//calculamos el costo total segun la cantidad de boletos
-								precioTotalBoleto := (20 * cantBoletos);
-								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
-								writeln('||------------------------------------------------------------||');
-							end;
-							
-							'3': begin
-								textcolor(Yellow);
-								writeln('||------------------------------------------------------------||');
-								writeln('|| Usted selecciono la opcion 3. Boleto Especial - Ida/vuelta.||');
-								//calculamos el costo total segun la cantidad de boletos
-								precioTotalBoleto := (24 * cantBoletos);
-								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
-								writeln('||------------------------------------------------------------||');
-							end;
-							
-							'4': begin
-								textcolor(Yellow);
-								writeln('||------------------------------------------------------------||');
-								writeln('|| Usted selecciono la opcion 4. Boleto Especial - Solo ida.  ||');
-								//calculamos el costo total segun la cantidad de boletos
-								precioTotalBoleto := (12 * cantBoletos);
-								writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
-								writeln('||------------------------------------------------------------||');
-							end;
+			begin	
+				repeat
+					clrscr;
+					textcolor(white);
+					writeln('||------------------------------------------||');
+					writeln('|| Seleccione la estacion de su preferencia ||');
+					writeln('||------------------------------------------||');
+					writeln('||                ESTACIONES                ||');
+					writeln('||------------------------------------------||');
+					writeln('|| 1. Barinitas                             ||');
+					writeln('|| 2. La montana                            ||');
+					writeln('|| 3. La Aguada                             ||');
+					writeln('|| 4. Loma Redonda                          ||');
+					writeln('|| 5. Pico Espejo                           ||');
+					writeln('||------------------------------------------||');
+					write('|| Indique el numero y presione ENTER: ');
+					readln(estaciones);
+					
+					case estaciones of
+						'1': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|***********************************************|');
+								writeln('| Usted ha seleccionado la estacion "Barinitas" |');
+								writeln('|***********************************************|');
+								delay(2000);
 						end;
 						
-						delay(5000);
-						clrscr;
-						writeln('||-----------------------------------------------------------------||');
-						writeln('|| Para continuar con su transaccion, ingrese los siguientes datos ||');
-						writeln('||-----------------------------------------------------------------||');
-						write('||Nombre: ');
-						readln(nombre);
+						'2': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|************************************************|');
+								writeln('| Usted ha seleccionado la estacion "La Montana" |');
+								writeln('|************************************************|');
+								delay(2000);
+						end;
 						
-						writeln(' ');
-						write('||Apellido: ');
-						readln(apellido);
+						'3': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|***********************************************|');
+								writeln('| Usted ha seleccionado la estacion "La Aguada" |');
+								writeln('|***********************************************|');
+								delay(1500);
+						end;
 						
-						writeln(' ');
-						write('||Numero de Cedula: ');
-						readln(cedula);
+						'4': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|**************************************************|');
+								writeln('| Usted ha seleccionado la estacion "Loma Redonda" |');
+								writeln('|**************************************************|');
+								delay(2000);
+						end;
 						
-						clrscr;
-						writeln('||--------------------------||');
-						writeln('||    DATOS DEL PASAJERO    ||');
-						writeln('||--------------------------||');
-						writeln('|| Nombre: ', nombre);
-						writeln('|| Apellido: ', apellido);
-						writeln('|| Numero de cedula: ', cedula);
-						writeln('||--------------------------||');
-						delay (3000);
+						'5': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('||-------------------------------------------------||');
+								writeln('|| Usted ha seleccionado la estacion "Pico Espejo" ||');
+								writeln('||-------------------------------------------------||');
+								delay(2000);
+						end;
+					end;
+				until (estaciones = '1') or (estaciones = '2') or (estaciones = '3') or (estaciones = '4') or (estaciones = '5');
+				
+				repeat
+					clrscr;
+					textcolor(white);
+					writeln(' ');
+					writeln('||------------------------------------------||');
+					writeln('|| Seleccione el tramo de su preferencia    ||');
+					writeln('||------------------------------------------||');
+					writeln('||                  TRAMOS                  ||');
+					writeln('||------------------------------------------||');
+					writeln('|| 1. Barinitas - La Montana                ||');
+					writeln('|| 2. La montana - La Aguada                ||');
+					writeln('|| 3. La Aguada - Loma Redonda              ||');
+					writeln('|| 4. Loma Redonda - Pico Espejo            ||');
+					writeln('||------------------------------------------||');
+					write('|| Indique el numero y presione ENTER: ');
+					readln(tramos);
+					
+					case tramos of
+						'1': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|*********************************************************|');
+								writeln('| Usted ha seleccionado el tramo "Barinitas - La Montana" |');
+								writeln('|*********************************************************|');
+								delay(2000);
+						end;
 						
+						'2': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|*********************************************************|');
+								writeln('| Usted ha seleccionado el tramo "La montana - La Aguada" |');
+								writeln('|*********************************************************|');
+								delay(2000);
+						end;
+						
+						'3': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|***********************************************************|');
+								writeln('| Usted ha seleccionado el tramo "La Aguada - Loma Redonda" |');
+								writeln('|***********************************************************|');
+								delay(2000);
+						end;
+						
+						'4': begin
+								writeln(' ');
+								textcolor(yellow);
+								writeln('|*************************************************************|');
+								writeln('| Usted ha seleccionado el tramo "Loma Redonda - Pico Espejo" |');
+								writeln('|*************************************************************|');
+								delay(2000);
+						end;
+					end;
+				until (tramos = '1') or (tramos = '2') or (tramos = '3') or (tramos = '4');
+				
+				clrscr;
+				textcolor(red);
+				writeln('||*****************************************************************||');
+				writeln('||   AVISO: La capacidad maxima de pasajeros por viaje es de ', cantMaxPasajeros, '    ||');
+				writeln('||*****************************************************************||');
+				
+				writeln(' ');
+				textcolor(white);
+				writeln('||-----------------------------------------------------------------||');
+				writeln('|| Para continuar con su transaccion, ingrese los siguientes datos ||');
+				writeln('||-----------------------------------------------------------------||');
+				write('||Nombre: ');
+				readln(nombre);
+				
+				writeln(' ');
+				write('||Apellido: ');
+				readln(apellido);
+				
+				writeln(' ');
+				write('||Numero de Cedula: ');
+				readln(cedula);
+
+				writeln(' ');
+				writeln('||---------------------------------------------||');
+				writeln('|| Viaja con personas de tercera edad o Ninos? ||');
+				writeln('|| 1. Si                                       ||');
+				writeln('|| 2. No                                       ||');
+				writeln('||---------------------------------------------||');
+				write('|| Indique su respuesta y presione ENTER: ');
+				readln(resp);
+				
+				case resp of
+					'1': begin
+						textcolor(white);
+						writeln('  ');
+						writeln('||---------------------------------------------||');
+						write('|| Ingrese la edad del nino/a y/o adulto mayor: ');
+						readln(edad); 
+						
+							if (edad >= 3) and (edad <= 12) or (edad >= 65) then
+								begin
+									writeln('  ');
+									writeln('||---------------------------------------------------------------------||');
+									writeln('|| Debe escoger el Boleto "Especial" para continuar con su transaccion ||');
+									writeln('||---------------------------------------------------------------------||');
+									delay(2000);
+								end
+							else 
+								if (edad > 12) then 
+									begin
+										writeln('||------------------------------------------------------||');
+										writeln('|| Los ninos/as mayores de 12 anos pagan Boleto General ||');
+										writeln('||------------------------------------------------------||');
+									end
+								else 
+									if (edad < 3) then
+										begin
+											textcolor(yellow);
+											writeln(' ');
+											writeln('||**********************************************************||');
+											writeln('|| LOS NINOS MENORES DE 3 ANOS QUEDAN TOTALMENTE EXONERADOS ||');
+											writeln('||**********************************************************||');
+											delay(2000);
+										end;
+					end;
+					'2': begin
+						writeln('  ');
+						writeln('||---------------------------------------------||');
+						writeln('||  Puede continuar con su transaccion normal  ||');
+						writeln('||---------------------------------------------||');
+						delay(2000);
+					end;
+				end;
+				
+				clrscr;
+				writeln('||---------------------------------------------||');
+				write('|| Cuantos boletos desea comprar? (MAXIMO 60): ');
+				readln(cantBoletos);
+
+				if (cantBoletos <= 60) then		
+					begin
 						repeat
 							clrscr;
-							writeln('||----------------------||');
-							writeln('||      ESTACIONES      ||');
-							writeln('||----------------------||');
-							writeln('|| 1. Barinitas         ||');
-							writeln('|| 2. La montana        ||');
-							writeln('|| 3. La Aguada         ||');
-							writeln('|| 4. Loma Redonda      ||');
-							writeln('|| 5. Pico Espejo       ||');
-							writeln('||----------------------||');
-							write('|| Indique la estacion de su preferencia y presione ENTER: ');
-							readln(estaciones);
-						until (estaciones = '1') or (estaciones = '2') or (estaciones = '3') or (estaciones = '4') or (estaciones = '5');
-						
-						case estaciones of
-							'1': begin
+							textcolor(white);
+							writeln('||---------------------------------------------||');
+							writeln('||          SELECCIONE EL TIPO DE BOLETO       ||');
+							writeln('||---------------------------------------------||');
+							writeln('|| 1. Boleto ida/vuelta  |  {PRECIO: $40.00}   ||');
+							writeln('|| 2. Boleto General     |  {PRECIO: $20.00}   ||');
+							writeln('|| 3. Boleto Especial    |  {PRECIO: $12.00}   ||');
+							writeln('|| 4. Ver Factura                              ||');
+							writeln('||---------------------------------------------||');
+							write('|| Ingrese el numero de su eleccion y presione ENTER: ');
+							readln(tipoBoleto);
+																	 
+							case tipoBoleto of
+								'1': begin
+									writeln('||--------------------------------------------------||');
+									writeln('||Usted selecciono la opcion 1. Boleto ida/vuelta.  ||');
+									//calculamos el costo total segun la cantidad de boletos
+									precioTotalBoleto := (40 * cantBoletos);
+									writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+									writeln('||--------------------------------------------------||');
+									delay(3000);
+								end;
+								
+								'2': begin
+									writeln('||---------------------------------------------||');
+									writeln('||Usted selecciono la opcion 2. Boleto General ||');
+									//calculamos el costo total segun la cantidad de boletos
+									precioTotalBoleto := (20 * cantBoletos);
+									writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+									writeln('||---------------------------------------------||');
+									delay(3000);
+								end;
+								
+								'3': begin
+									writeln('||-----------------------------------------------||');
+									writeln('|| Usted selecciono la opcion 3. Boleto Especial ||');
+									//calculamos el costo total segun la cantidad de boletos
+									precioTotalBoleto := (12 * cantBoletos);
+									writeln('|| Cuenta total: $', precioTotalBoleto:5:2); 
+									writeln('||-----------------------------------------------||');
+									delay(3000);
+								end;
+								
+								'4': begin
+									textcolor(lightgreen);
+									writeln('||*********************************************************||');
+									writeln('||                          FACTURA                        ||');
+									writeln('||*********************************************************||');
+									writeln('|| EMPRESA: Teleferico Merida.                             ||');
+									writeln('|| DIRECCION: Merida.                                      ||');
+									writeln('||*********************************************************||');
+									writeln('||  Nombre: ', (nombre));
+									writeln('||  Apellido: ', (apellido));
+									writeln('||  Cedula: ', (cedula));
+									writeln('||  Estacion: ', (estaciones));
+									writeln('||  Tramo: ', (tramos));
+									writeln('||  Boletos comprados: ', cantBoletos);
+									writeln('||  Tipo de Boletos: ', tipoBoleto);
+									writeln('||=========================================================||');
+									writeln('||                                 TOTAL A PAGAR->  ', montoTotal, '$');
+									writeln('||*********************************************************||');
+								end;
 							end;
 							
-							'2': begin
-							end;
-							
-							'3': begin
-							end;
-							
-							'4': begin
-							end;
-							
-							'5': begin
-							end;
-							
-						end; //END DEL CASE DENTRO DEL PRIMER IF
-					end
-					
+						until (tipoBoleto = '4');
+					end	
 					else if (cantBoletos > 60) then
 						begin
 							writeln(' ');
@@ -161,6 +311,6 @@ begin
 							
 							clrscr();
 						end;
-					end;
+				end;
 	until op = 3;
 end.
